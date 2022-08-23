46c807b4e483cd38fcdf1f9c4946afa884f16489
RJhuboo <88443856+RJhuboo@users.noreply.github.com>
2022-08-23 09:57:26 +0200

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -18,30 +18,28 @@ def normalization(csv_file,mode,indices):
     return scaler
 
 class Datasets(Dataset):
-    def __init__(self, csv_file, image_dir, opt, indices,transform=None):
-        self.opt = opt
+    def __init__(self,opt, csv_file, image_dir, indices, transform=None):
         self.image_dir = image_dir
         self.labels = pd.read_csv(csv_file)
         self.transform = transform
         self.indices = indices
+        self.opt = opt
+
     def __len__(self):
         return len(self.labels)
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
         img_name = os.path.join(self.image_dir, str(self.labels.iloc[idx,0]))
-        image = io.imread(img_name) # Loading Image
-        image = image / 255.0 # Normalizing [0;1]
-        image = image.astype('float32') # Converting images to float32
-        if self.opt["norm_method"]== "L2":
+        if self.opt['norm_method']== "L2":
             lab = preprocessing.normalize(self.labels.iloc[:,1:],axis=0)
-        elif self.opt["norm_method"] == "L1":
+        elif self.opt['norm_method'] == "L1":
             lab = preprocessing.normalize(self.labels.iloc[:,1:],norm='l1',axis=0)
-        elif self.opt["norm_method"] == "minmax":
+        elif self.opt['norm_method'] == "minmax":
             scaler = preprocessing.MinMaxScaler()
             scaler.fit(self.labels.iloc[self.indices,1:])
             lab = scaler.transform(self.labels.iloc[:,1:])
-        elif self.opt["norm_method"] == "standardization":
+        elif self.opt['norm_method'] == "standardization":
             scaler = preprocessing.StandardScaler()
             scaler.fit(self.labels.iloc[self.indices,1:])
             lab = scaler.transform(self.labels.iloc[:,1:])
@@ -51,9 +49,13 @@ class Datasets(Dataset):
         labels = lab.iloc[idx,1:] # Takes all corresponding labels
         labels = np.array([labels]) 
         labels = labels.astype('float32')
+        image = tio.Subject(ct=tio.ScalarImage(img_name+".nii.gz")) # Loading Image
         if self.transform:
-            sample = self.transform(sample)
-        return {'image': image, 'label': labels, 'ID': lab.iloc[idx,0]}
+            image = self.transform(image)
+        im = image['ct'][tio.DATA]
+        im = im.type(torch.FloatTensor)
+        print(im.type())
+        return {"image":im, "label":labels}
 
 class Test_Datasets(Dataset):
     def __init__(self, image_dir, transform=None):
