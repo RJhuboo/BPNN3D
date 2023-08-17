55b3cdf9b7e9b69b596dfdb653f9e01311f1e798
Rehan Jhuboo <rehan.jhuboo@univ-st-etienne.fr>
2023-08-17 13:09:59 +0200

    idx

--- dataloader.py
+++ dataloader.py
@@ -34,9 +34,10 @@ class Datasets(Dataset):
             idx = idx.tolist()
         #img_name = os.path.join(self.image_dir, str(self.labels.iloc[idx,0]))
         with h5py.File(self.image_dir,"r") as file_h5:
-            print(file_h5.keys())
+            #print(file_h5.keys())
+            print(idx)
             im = file_h5[str(idx)].astype(np.float32)
-            print(im[:,:,2])
+            #print(im[:,:,2])
             lab = self.scaler.transform(self.labels.iloc[:,1:])
             lab = pd.DataFrame(lab)
             lab.insert(0,"File name", self.labels.iloc[:,0], True)
