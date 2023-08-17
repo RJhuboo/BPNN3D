8750f667e64f23c9726e0e65de1875538df71b0c
Rehan Jhuboo <rehan.jhuboo@univ-st-etienne.fr>
2023-08-17 12:57:57 +0200

    search issue in dataloader

--- dataloader.py
+++ dataloader.py
@@ -35,7 +35,7 @@ class Datasets(Dataset):
         #img_name = os.path.join(self.image_dir, str(self.labels.iloc[idx,0]))
         with h5py.File(self.image_dir,"r") as file_h5:
             im = file_h5[str(idx)].astype(np.float32)
-            print(im)
+            print(im[:,:,2])
             lab = self.scaler.transform(self.labels.iloc[:,1:])
             lab = pd.DataFrame(lab)
             lab.insert(0,"File name", self.labels.iloc[:,0], True)
